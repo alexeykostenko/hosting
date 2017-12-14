c6795f62045c8e3fe845044e717804642030dda0
alexeykostenko <aleksey.kostenko.dev@gmail.com>
2017-12-14 23:51:31 +0200

    added dd helper

--- autoload.php
+++ autoload.php
@@ -4,3 +4,4 @@ require __DIR__ . '/Classes/Image.php';
 require __DIR__ . '/Classes/Request.php';
 require __DIR__ . '/Controller/ApiController.php';
 require __DIR__ . '/Model/Image.php';
+require __DIR__ . '/helpers.php';
