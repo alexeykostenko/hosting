842be16f325baab645b8b79556216f66e8df094e
alexeykostenko <aleksey.kostenko.dev@gmail.com>
2017-12-15 00:30:48 +0200

    added request helper

--- Classes/Request.php
+++ Classes/Request.php
@@ -4,13 +4,46 @@ namespace Hosting\Classes;
 
 class Request
 {
-    public function save()
-    {
+    /**
+     * Request body parameters ($_POST).
+     */
+    public $request;
 
+    /**
+     * Query string parameters ($_GET).
+     */
+    public $query;
+
+    /**
+     * Server and execution environment parameters ($_SERVER).
+     */
+    public $server;
+
+    /**
+     * Uploaded files ($_FILES).
+     */
+    public $files;
+
+    public function __construct()
+    {
+        $this->initialize();
     }
 
-    public function makeThumb()
+    public function initialize()
     {
+        $this->request = $_POST;
+        $this->query = $_GET;
+        $this->files = $_FILES;
+        $this->server = $_SERVER;
+    }
 
+    public function url()
+    {
+        return $this->server['REDIRECT_URL'];
+    }
+
+    public function fullUrl()
+    {
+        return $this->server['REQUEST_URI'];
     }
 }
--- helpers.php
+++ helpers.php
@@ -5,3 +5,8 @@ function dd()
     array_map(function($x) { var_dump($x); }, func_get_args());
     die();
 }
+
+function request()
+{
+    return new Hosting\Classes\Request;
+}
