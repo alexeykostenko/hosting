f15c1f1376130de1fae149a66e4aa5034d7ab8a8
alexeykostenko <aleksey.kostenko.dev@gmail.com>
2017-12-16 21:29:54 +0200

    added enctype=multipart/form-data in form

--- form.php
+++ form.php
@@ -4,7 +4,7 @@
         <title>form</title>
     </head>
     <body>
-        <form action="/images" method="post">
+        <form enctype="multipart/form-data" action="/images" method="post">
             <input name="_method" type="hidden" value="POST">
             <input type="file" name="images[]" title="Загрузите загрузить 1-5 изображений" required multiple>
             <button>submit</button>
